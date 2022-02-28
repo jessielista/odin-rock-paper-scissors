9522e4845d0a9e929c1411711cb79d8bdf10d393
jessielista <jmgonzalez8282@gmail.com>
2022-02-28 00:15:30 -0800

    User can play one round

--- app.js
+++ app.js
@@ -20,3 +20,56 @@ let playerSelection = prompt(
 console.log(`Player:${playerSelection}`)
 
 console.log(`Computer:${computerSelection}`)
+
+// Versus
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
+    return 'rock:it is a tie'
+  } else if (
+    playerSelection.toLowerCase() == 'paper' &&
+    computerSelection == 'paper'
+  ) {
+    return 'paper: it is a tie'
+  } else if (
+    playerSelection.toLowerCase() == 'scissors' &&
+    computerSelection == 'scissors'
+  ) {
+    return 'scissors: it is a tie'
+  } else if (
+    playerSelection.toLowerCase() == 'rock' &&
+    computerSelection == 'paper'
+  ) {
+    return 'Player chooses rock and Computer chooses paper. Computer wins!'
+  } else if (
+    playerSelection.toLowerCase() == 'rock' &&
+    computerSelection == 'scissors'
+  ) {
+    return 'Player chooses rock and Computer chooses scissors. Player wins!'
+  } else if (
+    playerSelection.toLowerCase() == 'paper' &&
+    computerSelection == 'scissors'
+  ) {
+    return 'Player chooses paper and Computer chooses scissors. Computer wins!'
+  } else if (
+    computerSelection.toLowerCase() == 'rock' &&
+    playerSelection == 'paper'
+  ) {
+    return 'Computer chooses rock and Player chooses paper. Player wins!'
+  } else if (
+    computerSelection.toLowerCase() == 'rock' &&
+    playerSelection == 'scissors'
+  ) {
+    return 'Computer chooses rock and Player chooses scissors. Computer wins!'
+  } else if (
+    computerSelection.toLowerCase() == 'paper' &&
+    playerSelection == 'scissors'
+  ) {
+    return 'Computer chooses paper and Player chooses scissors. Player wins!'
+  } else {
+    return 'Hey something is wrong!'
+  }
+  // should refactor
+}
+
+console.log(playRound(playerSelection, computerSelection))
