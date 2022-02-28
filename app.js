e92c3a44ac1aef5565d11b8f3bf12a7311adc8c0
jessielista <jmgonzalez8282@gmail.com>
2022-02-27 23:01:34 -0800

    Computer can play random move

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // console.log('Hello to me, Jessica.')
 
-// computerRandom = Math.random()
-// console.log(computerRandom)
+// first step is create list of random items, 3
+
+let playOptions = ['rock', 'paper', 'scissors']
+
+function computerPlay() {
+  computerRandomPlay = Math.floor(Math.random() * playOptions.length)
+  // console.log(computerRandomPlay)
+  let item = playOptions[computerRandomPlay]
+  return item
+}
+
+console.log(computerPlay())
